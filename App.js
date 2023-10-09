979da961a5fe84b552184ec98f9e2052fb5448fc
aungpyaephyo1412 <aungpyaephyo1412@gmail.com>
2023-10-09 09:49:27 +0630

    card

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import {StatusBar, StyleSheet, Text, TouchableOpacity, View} from 'react-native';
+import {ScrollView, StatusBar, StyleSheet, Text, TouchableOpacity, View} from 'react-native';
 import {FontAwesome5} from '@expo/vector-icons';
 
 export default function App() {
@@ -10,15 +10,174 @@ export default function App() {
                     <FontAwesome5 name="user-circle" size={30} color="black"/>
                 </TouchableOpacity>
             </View>
+            <ScrollView showsHorizontalScrollIndicator={false} showsVerticalScrollIndicator={false}>
+                <View style={styles.body}>
+                    <View style={styles.post}>
+                        <View style={styles.postHeader}>
+                            <FontAwesome5 name="user-circle" size={35} color="black"/>
+                            <View>
+                                <Text style={styles.postAuthor}>MG MG</Text>
+                                <Text style={styles.postDate}>13 321 3333</Text>
+                            </View>
+                        </View>
+                        <View>
+                            <Text style={styles.postBodyText}>
+                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad alias architecto deserunt dicta dolorum enim explicabo ipsa nemo nisi numquam, porro quaerat quo rem, rerum ullam voluptatibus voluptatum? Pariatur, recusandae.
+                            </Text>
+                        </View>
+                    </View>
+                    <View style={styles.post}>
+                        <View style={styles.postHeader}>
+                            <FontAwesome5 name="user-circle" size={35} color="black"/>
+                            <View>
+                                <Text style={styles.postAuthor}>MG MG</Text>
+                                <Text style={styles.postDate}>13 321 3333</Text>
+                            </View>
+                        </View>
+                        <View>
+                            <Text style={styles.postBodyText}>
+                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad alias architecto deserunt dicta dolorum enim explicabo ipsa nemo nisi numquam, porro quaerat quo rem, rerum ullam voluptatibus voluptatum? Pariatur, recusandae.
+                            </Text>
+                        </View>
+                    </View>
+                    <View style={styles.post}>
+                        <View style={styles.postHeader}>
+                            <FontAwesome5 name="user-circle" size={35} color="black"/>
+                            <View>
+                                <Text style={styles.postAuthor}>MG MG</Text>
+                                <Text style={styles.postDate}>13 321 3333</Text>
+                            </View>
+                        </View>
+                        <View>
+                            <Text style={styles.postBodyText}>
+                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad alias architecto deserunt dicta dolorum enim explicabo ipsa nemo nisi numquam, porro quaerat quo rem, rerum ullam voluptatibus voluptatum? Pariatur, recusandae.
+                            </Text>
+                        </View>
+                    </View>
+                    <View style={styles.post}>
+                        <View style={styles.postHeader}>
+                            <FontAwesome5 name="user-circle" size={35} color="black"/>
+                            <View>
+                                <Text style={styles.postAuthor}>MG MG</Text>
+                                <Text style={styles.postDate}>13 321 3333</Text>
+                            </View>
+                        </View>
+                        <View>
+                            <Text style={styles.postBodyText}>
+                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad alias architecto deserunt dicta dolorum enim explicabo ipsa nemo nisi numquam, porro quaerat quo rem, rerum ullam voluptatibus voluptatum? Pariatur, recusandae.
+                            </Text>
+                        </View>
+                    </View>
+                    <View style={styles.post}>
+                        <View style={styles.postHeader}>
+                            <FontAwesome5 name="user-circle" size={35} color="black"/>
+                            <View>
+                                <Text style={styles.postAuthor}>MG MG</Text>
+                                <Text style={styles.postDate}>13 321 3333</Text>
+                            </View>
+                        </View>
+                        <View>
+                            <Text style={styles.postBodyText}>
+                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad alias architecto deserunt dicta dolorum enim explicabo ipsa nemo nisi numquam, porro quaerat quo rem, rerum ullam voluptatibus voluptatum? Pariatur, recusandae.
+                            </Text>
+                        </View>
+                    </View>
+                    <View style={styles.post}>
+                        <View style={styles.postHeader}>
+                            <FontAwesome5 name="user-circle" size={35} color="black"/>
+                            <View>
+                                <Text style={styles.postAuthor}>MG MG</Text>
+                                <Text style={styles.postDate}>13 321 3333</Text>
+                            </View>
+                        </View>
+                        <View>
+                            <Text style={styles.postBodyText}>
+                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad alias architecto deserunt dicta dolorum enim explicabo ipsa nemo nisi numquam, porro quaerat quo rem, rerum ullam voluptatibus voluptatum? Pariatur, recusandae.
+                            </Text>
+                        </View>
+                    </View>
+                    <View style={styles.post}>
+                        <View style={styles.postHeader}>
+                            <FontAwesome5 name="user-circle" size={35} color="black"/>
+                            <View>
+                                <Text style={styles.postAuthor}>MG MG</Text>
+                                <Text style={styles.postDate}>13 321 3333</Text>
+                            </View>
+                        </View>
+                        <View>
+                            <Text style={styles.postBodyText}>
+                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad alias architecto deserunt dicta dolorum enim explicabo ipsa nemo nisi numquam, porro quaerat quo rem, rerum ullam voluptatibus voluptatum? Pariatur, recusandae.
+                            </Text>
+                        </View>
+                    </View>
+                    <View style={styles.post}>
+                        <View style={styles.postHeader}>
+                            <FontAwesome5 name="user-circle" size={35} color="black"/>
+                            <View>
+                                <Text style={styles.postAuthor}>MG MG</Text>
+                                <Text style={styles.postDate}>13 321 3333</Text>
+                            </View>
+                        </View>
+                        <View>
+                            <Text style={styles.postBodyText}>
+                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad alias architecto deserunt dicta dolorum enim explicabo ipsa nemo nisi numquam, porro quaerat quo rem, rerum ullam voluptatibus voluptatum? Pariatur, recusandae.
+                            </Text>
+                        </View>
+                    </View>
+                    <View style={styles.post}>
+                        <View style={styles.postHeader}>
+                            <FontAwesome5 name="user-circle" size={35} color="black"/>
+                            <View>
+                                <Text style={styles.postAuthor}>MG MG</Text>
+                                <Text style={styles.postDate}>13 321 3333</Text>
+                            </View>
+                        </View>
+                        <View>
+                            <Text style={styles.postBodyText}>
+                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad alias architecto deserunt dicta dolorum enim explicabo ipsa nemo nisi numquam, porro quaerat quo rem, rerum ullam voluptatibus voluptatum? Pariatur, recusandae.
+                            </Text>
+                        </View>
+                    </View>
+                    <View style={styles.post}>
+                        <View style={styles.postHeader}>
+                            <FontAwesome5 name="user-circle" size={35} color="black"/>
+                            <View>
+                                <Text style={styles.postAuthor}>MG MG</Text>
+                                <Text style={styles.postDate}>13 321 3333</Text>
+                            </View>
+                        </View>
+                        <View>
+                            <Text style={styles.postBodyText}>
+                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad alias architecto deserunt dicta dolorum enim explicabo ipsa nemo nisi numquam, porro quaerat quo rem, rerum ullam voluptatibus voluptatum? Pariatur, recusandae.
+                            </Text>
+                        </View>
+                    </View>
+                    <View style={styles.post}>
+                        <View style={styles.postHeader}>
+                            <FontAwesome5 name="user-circle" size={35} color="black"/>
+                            <View>
+                                <Text style={styles.postAuthor}>MG MG</Text>
+                                <Text style={styles.postDate}>13 321 3333</Text>
+                            </View>
+                        </View>
+                        <View>
+                            <Text style={styles.postBodyText}>
+                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad alias architecto deserunt dicta dolorum enim explicabo ipsa nemo nisi numquam, porro quaerat quo rem, rerum ullam voluptatibus voluptatum? Pariatur, recusandae.
+                            </Text>
+                        </View>
+                    </View>
+
+                </View>
+            </ScrollView>
         </View>
     );
 }
-
-const styles = StyleSheet.create({
+ const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: '#562aac',
-        paddingTop: StatusBar.currentHeight
+        paddingTop: StatusBar.currentHeight,
+        width:"100%",
+        maxWidth:"700"
     },
     header: {
         width: "100%",
@@ -33,4 +192,34 @@ const styles = StyleSheet.create({
         textTransform: "uppercase",
         fontWeight: "500"
     },
+    body:{
+        flex:1,
+        backgroundColor:"#eff8f9",
+        padding:5,
+    },
+    post:{
+        width:"100%",
+        backgroundColor:"#ffffff",
+        borderRadius:6,
+        elevation:3,
+        padding:10,
+        marginVertical:5
+    },
+     postHeader:{
+        flexDirection:"row",
+         gap:5,
+         alignItems:"center",
+         marginBottom:5
+     },
+     postAuthor:{
+        fontSize:15,
+         fontWeight:"700",
+         color:'#2f8f89'
+     },
+     postDate:{
+        color:"#6e6161"
+     },
+     postBodyText:{
+        color:"black"
+     }
 });
