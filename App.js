906807534d969efcf69e7e0a9c093adbaaaf4249
aungpyaephyo1412 <aungpyaephyo1412@gmail.com>
2023-10-09 09:42:57 +0630

    Revert "header"
    This reverts commit a8adc0295f7bc393ff65667491880ba6e0e7f281.

--- App.js
+++ App.js
@@ -1,36 +1,22 @@
-import {StatusBar, StyleSheet, Text, TouchableOpacity, View} from 'react-native';
-import {FontAwesome5} from '@expo/vector-icons';
+import { StyleSheet, Text, View } from 'react-native';
+import { AntDesign } from '@expo/vector-icons';
 
 export default function App() {
-    return (
-        <View style={styles.container}>
-            <View style={styles.header}>
-                <Text style={styles.headerText}>header</Text>
-                <TouchableOpacity onPress={()=>alert("hello")}>
-                    <FontAwesome5 name="user-circle" size={30} color="black"/>
-                </TouchableOpacity>
-            </View>
-        </View>
-    );
+  return (
+    <View style={styles.container}>
+      <Text style={{marginBottom:3,color:"white"}}>
+        Hello world
+      </Text>
+      <AntDesign name="infocirlce" size={24} color="white" />
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#562aac',
-        paddingTop: StatusBar.currentHeight
-    },
-    header: {
-        width: "100%",
-        backgroundColor: "lightblue",
-        flexDirection: "row",
-        alignItems: 'center',
-        justifyContent: 'space-between',
-        padding: 10,
-    },
-    headerText: {
-        fontSize: 20,
-        textTransform: "uppercase",
-        fontWeight: "500"
-    },
+  container: {
+    flex: 1,
+    backgroundColor: '#562aac',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
 });
